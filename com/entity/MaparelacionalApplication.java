0da0fba27efefcf19cda6395ec64302e0ead9ef4
raphex04 <132359802+raphex04@users.noreply.github.com>
2024-03-14 14:16:36 -0300

    Add files via upload

---
+++ com/entity/MaparelacionalApplication.java
@@ -0,0 +1,13 @@
+package com.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaparelacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaparelacionalApplication.class, args);
+	}
+
+}
